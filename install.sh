9baa27b7dbc829185eae1992f5db6bb37c31e1c3
André Missaglia <andre.missaglia@arquivei.com.br>
2018-10-15 11:36:06 -0300

    Adds DESTDIR support

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-mkdir /usr/share/kcompose
-cp ./kcompose.sh /usr/share/kcompose/
-cp -r ./kafka /usr/share/kcompose/kafka
-chmod +x /usr/share/kcompose/kcompose.sh
-ln -s /usr/share/kcompose/kcompose.sh /usr/bin/kcompose 
+mkdir -p $DESTDIR/usr/share/kcompose
+mkdir -p $DESTDIR/usr/bin
+cp ./kcompose.sh $DESTDIR/usr/share/kcompose/
+cp -r ./kafka $DESTDIR/usr/share/kcompose/kafka
+chmod +x $DESTDIR/usr/share/kcompose/kcompose.sh
+ln -sT /usr/share/kcompose/kcompose.sh $DESTDIR/usr/bin/kcompose
